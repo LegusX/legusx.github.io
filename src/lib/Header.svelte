aa9b5ebd93a58552b034641238474ce0b7dc92d2
Logan Henrie <loghenrie@gmail.com>
2022-03-07 22:23:18 -0500

    made header look good

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,37 +4,46 @@
 
 	const transPoint = 400
 
+
+	// transisition for header
 	onMount(()=>{
 		let header = document.getElementsByTagName("header")[0]
+		let height = 0
 
 		window.onscroll = ()=>{
-			console.log(window.div.getBoundingClientRect().top)
-			if (window.div.getBoundingClientRect().top < transPoint) {
-				header.style.backgroundColor = `rgba(30, 37, 205, ${(transPoint-window.div.getBoundingClientRect().top)/transPoint}`
-		}
-			if (window.div.getBoundingClientRect().top > transPoint) {
-				header.style.backgroundColor = `rgba(30, 37, 205, ${(transPoint-window.div.getBoundingClientRect().top)/transPoint}`
+			if (height === 0) {
+				height = "-"+window.getComputedStyle(window.div).marginTop
+				header.style.backgroundPositionY = height
 			}
+			if (window.div.getBoundingClientRect().top > header.getBoundingClientRect().height) {
+				header.classList.remove("bg-[url(/xbg.png)]")
+			}
+			else {
+				header.classList.add("bg-[url(/xbg.png)]")
+			}
+			// header.style.backgroundColor = `rgba(19, 20, 100, ${(transPoint-window.div.getBoundingClientRect().top)/transPoint}`
 		}
 	})
 </script>
 
-<div class="hidden bg-logo-blue/100">
-	I'm here just so classes get added :D
-</div>
-<header class="w-full flex bg-logo-blue/0 text-white items-center sticky top-0">
-	<div class="w-8 md:w-14 m-2 rounded-ful p-1 place-self-start">
+<header class="w-full flex text-white items-center sticky top-0">
+	<div class="w-14 m-2 rounded-ful p-1 place-self-start ">
 		<a href="/"><img src="/transparentx.png" alt="transparent legusx logo" class="rounded-full" ></a>
 	</div>
 	<div class="justify-end flex w-full">
 		<a href="/">
-			<div class="px-4 py-2 mr-8 {$page.url.pathname === "/" ? "rounded-md bg-white text-logo-blue":"ring-offset-1 ring-0 ring-white rounded-md"}">
+			<div class="px-4 py-2 mr-8 {$page.url.pathname === "/" ? "rounded-md bg-white text-logo-blue":"ring-offset-1 ring-0 ring-white rounded-md transition-all bg-white/0 hover:bg-logo-dark/40 hover:px-6"}">
 				Home
 			</div>
 		</a>
 		<a href="/projects">
-			<div class="px-4 py-2 mr-8 {$page.url.pathname === "/projects" ? "rounded-md bg-white text-logo-blue":"ring-offset-1 ring-0 ring-white rounded-md"}">
+			<div class="px-4 py-2 mr-8 {$page.url.pathname === "/projects" ? "rounded-md bg-white text-logo-blue":"ring-offset-1 ring-0 ring-white rounded-md transition-all bg-white/0 hover:bg-logo-dark/40 hover:px-6"}">
 				Projects
+			</div>
+		</a>
+		<a href="/about">
+			<div class="px-4 py-2 mr-8 {$page.url.pathname === "/about" ? "rounded-md bg-white text-logo-blue":"ring-offset-1 ring-0 ring-white rounded-md transition-all bg-white/0 hover:bg-logo-dark/40 hover:px-6"}">
+				About
 			</div>
 		</a>
 	</div>
